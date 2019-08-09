8a843e740d3edd20cb7d2ece7b8c342919527fee
kimhj <kimhj@gaia3d.com>
2019-08-09 16:08:35 +0900

    설정변경

--- src/main/java/demo/config/ServletConfig.java
+++ src/main/java/demo/config/ServletConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +18,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 					@Filter(type=FilterType.ANNOTATION, value=RestController.class)})
 public class ServletConfig implements WebMvcConfigurer {
 
-
+	/**
+	 * Cross-Origin Resouces Sharing
+	 * @return
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+			.allowedHeaders("*");
+	}
 }
